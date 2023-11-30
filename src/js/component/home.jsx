5c10ea6cfde73782c31fd436cd709c5cbaac9ba8
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2023-11-30 02:23:21 +0000

    commit 1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ selectedColor, setSelectedColor] = useState("yellow");
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container d-flex justify-content-center align-items-center">
+			<div className="traffic-light">
+				<div className="container bg-dark" style={{width:"70px", height:"185px"}}>
+					
+					<div >
+						<button 
+							type="button" 
+							onClick={() => setSelectedColor ("red")}
+							className={`light red btn btn-danger ${(selectedColor === "red") ? "glow" : ""}`} 
+							style={{width:"50px", height:"50px", borderRadius:"50px", marginTop:"5px", marginBottom:"5px"}}></button>
+					</div>
+					<div>
+						<button 
+							type="button" onClick={() => setSelectedColor ("yellow")}
+							className={`light yellow btn btn-warning ${(selectedColor === "yellow") ? "glow" : ""}`} 
+							style={{width:"50px", height:"50px", borderRadius:"50px", marginTop:"5px", marginBottom:"5px"}}></button>
+					</div>
+					<div>
+						<button 
+							type="button" onClick={() => setSelectedColor ("green")}
+							className={`light green btn btn-success ${(selectedColor === "green") ? "glow" : ""}`} 
+							style={{width:"50px", height:"50px", borderRadius:"50px", marginTop:"5px", marginBottom:"5px"}}></button>
+					</div>
+				</div>
+			</div>	
 		</div>
 	);
 };
